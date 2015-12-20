3239bd54e696bc6c5373ee456b18e1e276b77bd5
Daniel Hegner <daniel@hegner.se>
2015-12-20 18:26:44 +0100

    Added speech

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
+var request = require('request');
+var Lame = require('lame');
+var Speaker = require('speaker');
 var app = express();
 
 app.get('/', function (req, res) {
+  var text = process.env.TEXT || 'Say hello to my little friend';
+  var url = {
+	url: 'http://translate.google.com/translate_tts?ie=UTF-8&tl=en&client=t&q=' + encodeURIComponent(text),
+	headers: {
+		'User-Agent': 'stagefright/1.2 (Linux;Android 5.0)',
+		'Referer': 'http://translate.google.com/'
+  	}
+    };
+  request(url).pipe(new Lame.Decoder).pipe(new Speaker);
   res.send('Hello World!');
 });
 
